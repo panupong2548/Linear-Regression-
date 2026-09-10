6d46427eb108f32b0f70fe4d2d4f34b6079cd571
Panupong_W <panupong.wongjinda@gmail.com>
2026-09-10 13:03:02 +0700

    linear_Regression

---
+++ linear_Regression.py
@@ -0,0 +1,35 @@
+class linear_Regression:
+   
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.m = 10
+        self.b = 40
+        self.predictions = []
+        self.error = []
+
+    def predict(self):
+        for value in self.x:
+            prediction = self.m * value + self.b
+            self.predictions.append(prediction)
+        return self.predictions
+
+    def calculate_error(self):
+        for actual, predicted in zip(self.y, self.predictions):
+            error = actual - predicted
+            self.error.append(error**2)
+
+        mse = sum(self.error) / len(self.error)
+        return mse
+
+if __name__ == "__main__":
+    x = [1, 2, 3, 4, 5]
+    y = [50, 55, 65, 70, 80]
+
+    model = linear_Regression(x, y)
+    predictions = model.predict()
+    mse = model.calculate_error()
+
+    print("Predictions:", predictions)
+    print("Mean Squared Error:", mse)
+
